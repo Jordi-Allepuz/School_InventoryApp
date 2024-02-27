27fbf2d264d39b28371b14f57b642f94b8e7ea99
JordiAllepuz <valencianu@gmail.com>
2024-02-27 18:11:09 +0100

    Commit 1- Implementation (firebase, navigation, livedata, icons-extended, retrofit, coil)

--- app/src/main/java/com/example/school_inventoryapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/school_inventoryapp/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ private val LightColorScheme = lightColorScheme(
 fun School_InventoryAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
