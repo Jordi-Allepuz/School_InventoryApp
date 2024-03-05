0a54736fc8f46db77de751f87dcb02d74af8141f
JordiAllepuz <valencianu@gmail.com>
2024-03-05 23:44:21 +0100

    Commit4

--- app/src/main/java/com/example/school_inventoryapp/schoolinventoryapp/ui/screens/userinfo/components/UserInfo.kt
+++ app/src/main/java/com/example/school_inventoryapp/schoolinventoryapp/ui/screens/userinfo/components/UserInfo.kt
@@ -1,8 +1,10 @@
 package com.example.school_inventoryapp.schoolinventoryapp.ui.screens.userinfo.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Apartment
@@ -22,27 +24,47 @@ import com.example.school_inventoryapp.schoolinventoryapp.data.dataInfo.User
 
 @Composable
 fun UserInfo(user: User) {
-    Column() {
-        Row(modifier= Modifier.fillMaxWidth()) {
-            Icon(modifier= Modifier.size(50.dp),imageVector = Icons.Default.Email , contentDescription ="email" )
-            Column() {
-                Text(text= "Correo Electronico")
-                Text(text= user.email, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 40.dp), verticalArrangement= Arrangement.spacedBy(25.dp)) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Icon(
+                modifier = Modifier.size(50.dp),
+                imageVector = Icons.Default.Email,
+                contentDescription = "email"
+            )
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp)) {
+                Text(text = "Correo Electronico")
+                Text(text = user.email, fontSize = 20.sp, fontWeight = FontWeight.Bold)
             }
         }
-        Row(modifier= Modifier.fillMaxWidth()) {
-            Icon(modifier= Modifier.size(50.dp),imageVector =  Icons.Default.Apartment , contentDescription = "centro")
-            Column() {
-                Text(text= "Centro de trabajo")
-                Text(text= user.centro, fontSize = 20.sp, fontWeight = FontWeight.Bold )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Icon(
+                modifier = Modifier.size(50.dp),
+                imageVector = Icons.Default.Apartment,
+                contentDescription = "centro"
+            )
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp)) {
+                Text(text = "Centro de trabajo")
+                Text(text = user.centro, fontSize = 20.sp, fontWeight = FontWeight.Bold)
             }
 
         }
-        Row(modifier= Modifier.fillMaxWidth()) {
-            Icon(modifier= Modifier.size(50.dp),imageVector = Icons.Default.School, contentDescription = "clase" )
-            Column() {
-                Text(text= "Curso")
-                Text(text= user.clase, fontSize = 20.sp, fontWeight = FontWeight.Bold )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Icon(
+                modifier = Modifier.size(50.dp),
+                imageVector = Icons.Default.School,
+                contentDescription = "clase"
+            )
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 20.dp)) {
+                Text(text = "Curso")
+                Text(text = user.clase, fontSize = 20.sp, fontWeight = FontWeight.Bold)
             }
 
         }
